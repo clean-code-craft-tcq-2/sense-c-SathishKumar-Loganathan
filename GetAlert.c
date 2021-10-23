204090764a391814d2cc729b48539c0912f7df3b
SathishKumar Loganathan <43195344+SathishKumar-Loganathan@users.noreply.github.com>
2021-10-23 16:11:57 +0530

    Update GetAlert.c

--- GetAlert.c
+++ GetAlert.c
@@ -13,3 +13,11 @@ void ledAlerter(void)
     ledAlertCallCount++;
 }
 
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+    if(computedStats.max > maxThreshold)
+    {
+        alerters[0]();
+        alerters[1]();
+    }
+}
